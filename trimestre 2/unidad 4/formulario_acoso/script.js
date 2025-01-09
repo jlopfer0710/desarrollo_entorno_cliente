5aea3a471beaf1592eb3c49f10047906d3d2962e
jlopfer0710 <jorgelopezfernandez93@gmail.com>
2025-01-09 18:43:52 +0100

    Add files via upload

--- trimestre 2/unidad 4/formulario_acoso/script.js
+++ trimestre 2/unidad 4/formulario_acoso/script.js
@@ -1,14 +1,35 @@
+
 function enviar(event) {
     event.preventDefault();
+    var mensaje=document.getElementById("mensaje");
     var cont=0;
+    var cont_respuestas=0;
     var valor=document.getElementsByTagName("input");
     for(var i=0;i<valor.length;i++){
         if(valor[i].checked){
-            cont+=parseInt(valor[i].value);
+            cont++;
+            if(valor[i].value==1){
+                cont_respuestas+=parseInt(valor[i].value);
+            }
         }
     }
-    if(cont==10){
-        alert("Sufres acoso");
+    if(cont_respuestas==0){
+        mensaje.innerHTML="<strong>No sufres acoso.</strong>";
+    }else if(cont_respuestas >=1 &&cont_respuestas<=5){
+        mensaje.innerHTML="<strong>Hay indicios de acoso actue con discreción.</strong>";
+    }else if(cont_respuestas>=6 &&cont_respuestas<=10){
+        mensaje.innerHTML="<strong>Sufres acoso, por favor, contacte con sus padres/tutores legales y con su profesorado. </strong>";
     }
-    alert("Hola el botón fue presionado");
+    if(cont<10){
+        alert("No puedes dejarte preguntas sin responder");
+        mensaje.innerHTML="";
+        return;
+    }else{
+        alert("Formulario enviado correctamente");
+        document.getElementById("form").submit();
+        
+    }
 }
+var form=document.getElementById("form");
+form.addEventListener('submit',enviar);
+
